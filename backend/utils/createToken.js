1a0d186e578837697702bc73e148c1849a541c68
obsideen <97191699+Pritthish20@users.noreply.github.com>
2024-12-20 03:12:28 +0530

    Cloudinary Commit 4

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -7,7 +7,7 @@ const generateToken=(res,userId)=> {
     // console.log("Token generated",token);
     // set jwt as http-only cookie
     res.cookie("jwt",token,{
-        httpOnly: true,
+        // httpOnly: true,
         secure: true,
         sameSite: "none",
         maxAge: 30*24*60*60*1000,
